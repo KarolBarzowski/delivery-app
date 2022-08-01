f9cef2ae790cd27aa941d3b3ae20ccd919fbde10
KarolBarzowski <gamekarol@gmail.com>
2022-08-01 21:49:21 +0200

    Added Login view

--- frontend/src/views/Root.tsx
+++ frontend/src/views/Root.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 // import Home from 'views/Home';
 import RestaurantDashboard from 'views/RestaurantDashboard';
 import NotFound from 'views/NotFound';
+import Login from 'views/Login';
 
 function Root() {
   return (
     <Routes>
       <Route path="/" element={<RestaurantDashboard />} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
