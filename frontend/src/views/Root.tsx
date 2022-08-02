347942f4726930523bc87ca4b0c0c2e2b810ef75
KarolBarzowski <gamekarol@gmail.com>
2022-08-02 20:02:11 +0200

    First msw implementation

--- frontend/src/views/Root.tsx
+++ frontend/src/views/Root.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-// import Home from 'views/Home';
-import RestaurantDashboard from 'views/RestaurantDashboard';
+import Home from 'views/Home';
+// import RestaurantDashboard from 'views/RestaurantDashboard';
 import NotFound from 'views/NotFound';
 import Login from 'views/Login';
 
 function Root() {
   return (
     <Routes>
-      <Route path="/" element={<RestaurantDashboard />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
