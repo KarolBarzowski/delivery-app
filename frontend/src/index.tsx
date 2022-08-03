16f075f79b3e800e2faf418c7563c3e5580caf81
KarolBarzowski <gamekarol@gmail.com>
2022-08-03 11:54:18 +0200

    Firefox fix

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,17 +5,17 @@ import AppProviders from 'providers/AppProviders';
 import reportWebVitals from './reportWebVitals';
 import { worker } from 'mocks/browser';
 
-worker.start();
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
-root.render(
-  <React.StrictMode>
-    <AppProviders>
-      <Root />
-    </AppProviders>
-  </React.StrictMode>
-);
+worker.start().then(() => {
+  root.render(
+    <React.StrictMode>
+      <AppProviders>
+        <Root />
+      </AppProviders>
+    </React.StrictMode>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
